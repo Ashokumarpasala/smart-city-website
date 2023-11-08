510cef686038910f9a1c5730f4f8a23996864a22
Ashokumarpasala <adinarayanapasala9Wgmail.com>
2023-11-08 12:33:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//.....Ternary Operator
+let age = 20;
+// variable = condition ? Exp1 : Exp2 ;
+let voter = (age >= 18) ? 'Eligible to Vote' : 'Not Eligible to Vote';
+
+console.log(voter); // Output: 'Eligible to Vote'
